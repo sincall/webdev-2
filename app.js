c13770870862aa4ab5408f628661469fcd4b9288
sincall <48347976+sincall@users.noreply.github.com>
2019-05-17 17:50:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log("Hello World!");
 alert("HEllo1");
+
+var target = document.getElementById("target");
+target.innerText = "hi!";
+
+var button = document.getElementById("button");
+button.addEventListener('click', function(){
+    alert("yes, hi sister")
+});
